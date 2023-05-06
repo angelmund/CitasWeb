264372a36260f9bc3e8c82ee12251e7db4b41323
Angel <197o00699@itsx.edu.mx>
2023-05-06 09:19:13 -0600

    cambio en mostrar

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -1,4 +1,4 @@
-let paso=1;
+let paso=1; //indica el paso que se quiere mostrar, en este caso el paso 1 es el que se muestra
 
 document.addEventListener('DOMContentLoaded', function(){
     iniciarApp();
@@ -6,12 +6,19 @@ document.addEventListener('DOMContentLoaded', function(){
 
 function iniciarApp(){
     tabs(); //cambia la secci[on] cuando se presiona el tabs
-    
+    mostrarSeccion(); //muestra y oculta las secciones
 }
 //hace que se muestre una seccion al dar click en el buttton
 function mostrarSeccion(){
+    //Ocultar la seccion que tenga la clase mostrar
+    const seccionAnterior = document.querySelector('.mostrar'); //lleva .mostrar solo cuando es el selector
+    if(seccionAnterior){
+        seccionAnterior.classList.remove('mostrar'); //si encunetra una seccion mostrar, oculta, si no,no hace nada 
+    }
+    // aqui termina Ocultar la seccion que tenga la clase mostrar
+
     //selecciona la seccion con el paso
-    const pasoSelector = '#paso-${paso}';
+    const pasoSelector = `#paso-${paso}`;
     const seccion = document.querySelector(pasoSelector);
     seccion.classList.add('mostrar');
 }
